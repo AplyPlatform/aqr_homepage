bf54e4b7df14e39782f864230eaa30806c879297
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2024-08-09 18:42:20 +0900

    스크롤시 하단 버튼 보이게끔 수정

--- assets/js/handler.js
+++ assets/js/handler.js
@@ -248,6 +248,23 @@ function getCookie(cName) {
   return matches ? matches[1] : "";
 }
 
+function stickToTop() {    
+	let ntop = 65;    
+    let winScrollTop = $(window).scrollTop();
+
+    if (winScrollTop >= ntop) {
+        $('#bottom_smartstore_button').show();
+		$('#bottom_register_button').show();
+    } else {
+        $('#bottom_smartstore_button').hide();
+		$('#bottom_register_button').hide();
+    }
+}
+
 $(function() {
+	$(window).on('scroll', function() {        
+        stickToTop();
+    });
+
 	$('[data-youtube]').youtube_background();
 });
