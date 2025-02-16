bdfab77634f8bd4b05173519178efad824d9ec15
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2025-02-16 12:20:26 +0900

    고급 고유 URL 신청 폼이 자동 입력 안되는 오류 수정

--- assets/js/handler.js
+++ assets/js/handler.js
@@ -39,11 +39,20 @@ $(function () {
 
     $('[data-youtube]').youtube_background();
 
+    $("#form_contact_min_type_2").click(function () {
+        let isChecked = $(this).is(":checked");
+		if (isChecked) {
+		  let str = $("#form_contact_content").val();
+		  if (str.includes("고급 고유 URL") && str.includes("비영리 공익/사회적 기업 및 단체") && str.includes("고급 고유 URL을 위한 키워드 후보 1")) return;
+		  $("#form_contact_content").val("* 고급 고유 URL은 비영리 공익/사회적 기업 및 단체, 제휴 기업에게 무상으로 제공합니다.\n\n-고유 URL :\n-기업/단체명 :\n-고급 고유 URL을 위한 키워드 후보 1 Ex) mycompany1 :\n-고급 고유 URL을 위한 키워드 후보 2 Ex) mycompany2 :\n" + $("#form_contact_content").val());
+		}
+    });
+
     if (AAPI_isSet(getCookie("ref1"))) return;
     if (AAPI_isSet(document.referrer) == false) {
         setCookie("ref1", "", 1)
-        return;
     }
-
-    setCookie("ref1", document.referrer, 1);
+    else {
+        setCookie("ref1", document.referrer, 1);
+    }        
 });
