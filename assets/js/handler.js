33034f7d1131f58fc0a3b657c7dd5744c74b59f1
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2026-04-08 20:14:19 +0900

    종교단체 제외 문구 추가

--- assets/js/handler.js
+++ assets/js/handler.js
@@ -43,7 +43,7 @@ $(function () {
 		if (isChecked) {
 		  let str = $("#form_contact_content").val();
 		  if (str.includes("고급 고유 URL") && str.includes("비영리 공익/사회적 기업 및 단체") && str.includes("고급 고유 URL을 위한 키워드 후보 1")) return;
-		  $("#form_contact_content").val("* 고급 고유 URL은 비영리 공익/사회적 기업 및 단체, 제휴 기업에게 무상으로 제공합니다.\n\n-고유 URL :\n-기업/단체명 :\n-고급 고유 URL을 위한 키워드 후보 1 Ex) mycompany1 :\n-고급 고유 URL을 위한 키워드 후보 2 Ex) mycompany2 :\n" + $("#form_contact_content").val());
+		  $("#form_contact_content").val("* 고급 고유 URL은 비영리 공익/사회적 기업 및 단체(종교단체 제외), 제휴 기업에게 무상으로 제공합니다.\n\n-고유 URL :\n-기업/단체명 :\n-고급 고유 URL을 위한 키워드 후보 1 Ex) mycompany1 :\n-고급 고유 URL을 위한 키워드 후보 2 Ex) mycompany2 :\n" + $("#form_contact_content").val());
 		}
     });
     
